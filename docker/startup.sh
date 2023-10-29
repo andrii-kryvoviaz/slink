2ebc84739d6f42e6ee97b23eae4ca4fc55cdc354
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2023-10-29 21:54:44 +0200

    Add Event Store migrations and fix issues

--- docker/startup.sh
+++ docker/startup.sh
@@ -7,4 +7,5 @@
 /app/bin/console doctrine:database:create --if-not-exists --no-interaction --env=prod --connection=event_store
 
 # Apply migrations
-/app/bin/console doctrine:migrations:migrate --no-interaction
+/app/bin/console doctrine:migrations:migrate --no-interaction --configuration=/app/config/migrations/event_store.yaml --em=event_store
+/app/bin/console doctrine:migrations:migrate --no-interaction --em=read_model
--- src/Slik/Shared/Infrastructure/Persistence/Doctrine/Migrations/Version20230925213206.php
+++ src/Slik/Shared/Infrastructure/Persistence/Doctrine/Migrations/Version20230925213206.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace DoctrineMigrations;
+namespace Slik\Shared\Infrastructure\Persistence\Doctrine\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
