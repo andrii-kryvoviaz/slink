c5aec3dc2eaa61c628679821f751424a40e60a85
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2023-11-04 17:52:48 +0200

    Extension bug fix

--- src/UI/Http/Rest/Controller/Image/GetImageController.php
+++ src/UI/Http/Rest/Controller/Image/GetImageController.php
@@ -9,6 +9,7 @@ use Slik\Image\Application\Query\GetImageById\GetImageByIdQuery;
 use Slik\Image\Application\Query\GetImageContent\GetImageContentQuery;
 use Slik\Shared\Application\Command\CommandTrait;
 use Slik\Shared\Application\Query\QueryTrait;
+use Slik\Shared\Infrastructure\Exception\NotFoundException;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use UI\Http\Rest\Response\ContentResponse;
@@ -19,10 +20,18 @@ final readonly class GetImageController {
   use CommandTrait;
   use QueryTrait;
   
-  public function __invoke(string $id, string $projectPublicDir): ContentResponse {
+  /**
+   * @throws NotFoundException
+   */
+  public function __invoke(string $id, string $ext): ContentResponse {
     $this->handle(new AddImageViewCountCommand($id));
     
     $imageView = $this->ask(new GetImageByIdQuery($id, false));
+    
+    if($imageView->getAttributes()->getFileName() !== "$id.$ext") {
+      throw new NotFoundException();
+    }
+    
     $imageData = $this->ask(new GetImageContentQuery($imageView->getAttributes()->getFileName()));
     
     return ContentResponse::file($imageData, $imageView->getMetadata()->getMimeType());
