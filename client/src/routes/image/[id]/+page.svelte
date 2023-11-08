7bea5861b4416bdd83fa18e8367f091714e67f77
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2023-11-09 00:38:20 +0200

    Fix image page layout

--- client/src/routes/image/[id]/+page.svelte
+++ client/src/routes/image/[id]/+page.svelte
@@ -6,14 +6,16 @@
 </script>
 
 <div class="flex justify-center p-8">
-  <div class="container flex max-w-[1280px] flex-col gap-6 lg:flex-row">
+  <div
+    class="container flex max-w-[1280px] flex-col items-center gap-6 lg:flex-row lg:items-start"
+  >
     <ImagePlaceholder
       src="/api/{data.url}"
       height={32}
       aspectRatio={data.height / data.width}
       metadata={data}
     />
-    <div class="px-2 py-8">
+    <div class="w-full px-2 py-8">
       <h1
         class="border-l-4 border-slate-800 pl-2 text-xl font-bold text-slate-900 dark:border-slate-400 dark:text-slate-100"
       >
