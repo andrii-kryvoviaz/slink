62e89695732a57200b0a99c1dbcd5c9dfd18df25
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2023-11-13 19:12:40 +0200

    Add responsive screen sizes to tailwind.config.ts.

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -10,7 +10,16 @@ export default {
     './node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}',
   ],
   theme: {
-    extend: tailwindcssTheme
+    extend: tailwindcssTheme,
+    screens: {
+      xs: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+      '3xl': '1920px',
+    },
   },
   plugins: [ThemeCustomizerPlugin(defaultTheme)],
 } satisfies Config;
