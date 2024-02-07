b80e3bb5efb63b4e56a5e0c32471774d7b02af09
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-07 12:51:55 +0200

    Refactor AbstractCompoundValueObject to extend AbstractValueObject

--- src/Slik/Shared/Domain/ValueObject/AbstractCompoundValueObject.php
+++ src/Slik/Shared/Domain/ValueObject/AbstractCompoundValueObject.php
@@ -4,20 +4,16 @@ declare(strict_types=1);
 
 namespace Slik\Shared\Domain\ValueObject;
 
-abstract readonly class AbstractCompoundValueObject {
+abstract readonly class AbstractCompoundValueObject extends AbstractValueObject{
   abstract public function toPayload(): array;
   
   abstract public static function fromPayload(array $payload): static;
   
-  public function equals(self $other): bool {
-    return $this->toPayload() === $other->toPayload();
-  }
-  
-  public function __toString(): string {
-    return json_encode($this->toPayload());
-  }
-  
   public function merge(self $other): self {
     return static::fromPayload(array_merge($this->toPayload(), $other->toPayload()));
+  }
+  
+  public function toString(): string {
+    return json_encode($this->toPayload());
   }
 }
--- src/Slik/Shared/Domain/ValueObject/AbstractValueObject.php
+++ src/Slik/Shared/Domain/ValueObject/AbstractValueObject.php
@@ -6,6 +6,18 @@ namespace Slik\Shared\Domain\ValueObject;
 
 abstract readonly class AbstractValueObject implements \JsonSerializable, \Stringable {
   abstract public function toString(): string;
+  
+  public function equals(self $other): bool {
+    $properties = get_object_vars($this);
+    
+    foreach ($properties as $property => $value) {
+      if ($value !== $other->{$property}) {
+        return false;
+      }
+    }
+    
+    return true;
+  }
 
   public function __toString(): string {
     return $this->toString();
