bc55e68d3d538e6fa703c686b8ecd55ccb07a293
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-08 14:30:48 +0200

    Add exception handling for image not found

--- src/Slik/Image/Application/Query/GetImageContent/GetImageContentHandler.php
+++ src/Slik/Image/Application/Query/GetImageContent/GetImageContentHandler.php
@@ -6,12 +6,16 @@ namespace Slik\Image\Application\Query\GetImageContent;
 
 use Slik\Shared\Application\Http\Item;
 use Slik\Shared\Application\Query\QueryHandlerInterface;
+use Slik\Shared\Infrastructure\Exception\NotFoundException;
 use Slik\Shared\Infrastructure\FileSystem\Storage\StorageInterface;
 
 final readonly class GetImageContentHandler implements QueryHandlerInterface {
   public function __construct(private StorageInterface $storage) {
   }
   
+  /**
+   * @throws NotFoundException
+   */
   public function __invoke(GetImageContentQuery $query): Item {
     $imageContent = $this->storage->getImage($query->getImageOptions());
     
