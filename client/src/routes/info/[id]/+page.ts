44dc485dcbbb74ae93369603f9c388fa3da38f7d
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-09 17:37:12 +0200

    Update import statement in +page.ts

--- client/src/routes/info/[id]/+page.ts
+++ client/src/routes/info/[id]/+page.ts
@@ -1,9 +1,6 @@
-import { ApiClient } from '../../../api/Client';
-
 import type { PageLoad } from './$types';
+import { ApiClient } from '@slink/api/Client';
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  const response = await ApiClient.image.using(fetch).getDetails(params.id);
-
-  return response;
+  return await ApiClient.image.using(fetch).getDetails(params.id);
 };
