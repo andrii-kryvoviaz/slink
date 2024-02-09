31e51e99ea3cd0d8606ed6da93b7b4e44b6a1c9d
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-09 17:25:44 +0200

    Add CookieStore class cookie subscription

---
+++ client/src/lib/store/cookieStore.ts
@@ -0,0 +1,22 @@
+import type { Invalidator, Readable, Subscriber, Unsubscriber } from 'svelte/store';
+import { cookie } from '@slink/utils/cookie';
+
+class CookieStore<T> implements Readable<T> {
+  constructor(private readonly cookieName: string, private readonly defaultValue?: T) {
+  }
+
+  public subscribe(run: Subscriber<T>, invalidate?: Invalidator<T>): Unsubscriber {
+    run(cookie.get(this.cookieName, this.defaultValue as unknown as string) as T);
+
+    const listener = (value: string) => run(value as unknown as T);
+    let listenerId = cookie.subscribe(this.cookieName, listener);
+
+    return () => {
+      cookie.unsubscribe(this.cookieName, listenerId);
+    };
+  }
+}
+
+export function cookieReadableStore<T>(cookieName: string, defaultValue?: T): Readable<T> {
+  return new CookieStore<T>(cookieName, defaultValue);
+}
