e9bf626b690a9fdc6d5e3b95237823526c7b0bd6
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-09 17:30:10 +0200

    Refactor ThemeSwitch component

--- client/src/components/Shared/Action/ThemeSwitch.svelte
+++ client/src/components/Shared/Action/ThemeSwitch.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
   import Icon from '@iconify/svelte';
-  import { isDarkTheme, setTheme } from '@slink/store/settings';
-  import { onMount } from 'svelte';
+  import { isDarkTheme, setTheme, Theme } from '@slink/store/settings';
 
   export let disabled = false;
-  let postponedLoad = true;
 
   const toggleTheme = (e: Event) => {
-    setTheme((e.target as HTMLInputElement).checked ? 'dark' : 'light');
+    setTheme((e.target as HTMLInputElement).checked ? Theme.DARK : Theme.LIGHT);
   };
-
-  onMount(() => {
-    postponedLoad = false;
-  });
 </script>
 
-{#if !disabled && !postponedLoad}
+{#if !disabled}
   <label class="flex cursor-pointer gap-2 items-center">
     <Icon icon="ph:sun-thin" width="20" height="20"/>
     <input type="checkbox" class="toggle theme-controller" on:change={toggleTheme} checked={$isDarkTheme} />
