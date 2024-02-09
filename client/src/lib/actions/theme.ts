5e52a2d867ead66be87c1b32634240efcc1bb954
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-09 17:29:34 +0200

    Add theme action to handle changing the theme

---
+++ client/src/lib/actions/theme.ts
@@ -0,0 +1,23 @@
+import type { Action } from "svelte/action";
+import { Theme } from '@slink/store/settings';
+
+export const theme: Action<HTMLElement, string> = (node: HTMLElement, themeName: string) => {
+  if(node !== document.documentElement) {
+    node = document.documentElement;
+  }
+
+  const update = (t: string) => {
+    if(!node.classList.contains(t)) {
+      node.classList.remove(...Object.values(Theme));
+    }
+
+    node.classList.add(t);
+  }
+
+  return {
+    update,
+    destroy() {
+      node.classList.remove(themeName);
+    }
+  };
+};
