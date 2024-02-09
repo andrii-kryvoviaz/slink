d567a068ea842b286bf5241308f8e1deb37c20cb
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-09 17:29:11 +0200

    Update app.html and hooks.server.ts to support SSR theming

--- client/src/hooks.server.ts
+++ client/src/hooks.server.ts
@@ -1,27 +1,24 @@
 import type { Handle } from '@sveltejs/kit';
+import { cookie } from '@slink/utils/cookie';
+import { Theme } from '@slink/store/settings';
 
 const API_BASE_URL = 'http://localhost:8080';
 const PROXY_PATH = '/api';
 
 const handleApiProxy: Handle = async ({ event }) => {
-  // Ensure that PROXY_PATH is escaped properly for use in a regex pattern:
   const escapedProxyPath = PROXY_PATH.replace(/[-[/\]{}()*+?.\\^$|]/g, '\\$&');
 
-  // Create a RegExp object with the escaped proxy path:
   const proxyPathRegex = new RegExp(`^${escapedProxyPath}`);
 
   // strip `/api` from the request path before proxying
   const strippedPath = event.url.pathname.replace(proxyPathRegex, '');
 
-  // build the new URL path with your API base URL, the stripped path and the query string
   const urlPath = `${API_BASE_URL}${strippedPath}${event.url.search}`;
   const proxiedUrl = new URL(urlPath);
 
-  // Strip off header added by SvelteKit yet forbidden by underlying HTTP request
   event.request.headers.delete('connection');
 
   const options: any = {
-    // propagate the request method and body
     body: event.request.body,
     method: event.request.method,
     headers: event.request.headers,
@@ -38,13 +35,16 @@ const handleApiProxy: Handle = async ({ event }) => {
 };
 
 export const handle: Handle = async ({ event, resolve }) => {
-  // intercept requests
   const paths = [PROXY_PATH, '/image'];
   const pathRegex = new RegExp(`^(${paths.join('|')})`);
+
+  cookie.setServerCookies(event.cookies || []);
 
   if (pathRegex.test(event.url.pathname)) {
     return handleApiProxy({ event, resolve });
   }
 
-  return resolve(event);
+  return resolve(event,  {
+    transformPageChunk: ({ html }) => html.replace('%app.theme%', cookie.get('theme', Theme.DARK))
+  });
 };
