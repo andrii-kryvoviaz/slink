636f041ede888c4f254545ed9eae7e210ea7cfa3
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-09 18:30:09 +0200

    Add tooltips to Navbar buttons

--- client/src/components/Layout/Navbar.svelte
+++ client/src/components/Layout/Navbar.svelte
@@ -2,6 +2,7 @@
   import Button from '@slink/components/Shared/Action/Button.svelte';
   import Icon from '@iconify/svelte';
   import ThemeSwitch from '@slink/components/Shared/Action/ThemeSwitch.svelte';
+  import { Tooltip } from 'flowbite-svelte';
 
 </script>
 
@@ -15,12 +16,39 @@
     </div>
 
     <div class="hidden lg:flex lg:flex-1 lg:justify-end lg:items-center gap-2">
-      <Button href='/upload' variant='invisible' size='sm' class="rounded-full">
+      <Button
+        href="/upload"
+        variant="link"
+        size="sm"
+        rounded="full"
+        id="uploadImageLink"
+      >
         <Icon icon="simple-line-icons:plus" class="h-5 w-5" />
       </Button>
-      <Button href='https://github.com/andrii-kryvoviaz/slink' variant='link' target='_blank'>
+      <Tooltip
+        triggeredBy="[id^='uploadImageLink']"
+        class="max-w-[10rem] p-2 text-center text-xs"
+        color="dark"
+        placement="bottom"
+      >
+        Upload an image
+      </Tooltip>
+      <Button
+        href='https://github.com/andrii-kryvoviaz/slink'
+        variant='link'
+        target='_blank'
+        id="projectLink"
+      >
         <Icon icon="akar-icons:github-fill" class="h-5 w-5" />
       </Button>
+      <Tooltip
+        triggeredBy="[id^='projectLink']"
+        class="max-w-[10rem] p-2 text-center text-xs"
+        color="dark"
+        placement="bottom"
+      >
+        Project on GitHub
+      </Tooltip>
       <ThemeSwitch />
       <Button href='/profile/login' variant='link'>
         <span class="text-sm font-semibold leading-6">Log in <span aria-hidden="true">&rarr;</span></span>
