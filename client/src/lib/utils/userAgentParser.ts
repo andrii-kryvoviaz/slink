f7e9996e5cc5bed077766e6dc4e2d2d4c79904e9
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-13 23:11:52 +0200

    Add user agent parsing functionality

---
+++ client/src/lib/utils/userAgentParser.ts
@@ -0,0 +1,11 @@
+import UAParser from 'ua-parser-js';
+
+export const parseUserAgent = (userAgent: string) => {
+  const ua = new UAParser(userAgent);
+  return ua.getResult();
+}
+
+export const parseUserAgentFromRequest = (request: Request) => {
+  const userAgents = request.headers.get('user-agent');
+  return parseUserAgent(userAgents || '');
+}
