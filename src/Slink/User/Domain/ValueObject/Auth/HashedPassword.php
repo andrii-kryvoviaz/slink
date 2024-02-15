fe5c06d08bc048cf670d990cd419368ed80bf0a7
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-15 20:05:46 +0200

    Refactor HashedPassword class to extend AbstractValueObject

--- src/Slink/User/Domain/ValueObject/Auth/HashedPassword.php
+++ src/Slink/User/Domain/ValueObject/Auth/HashedPassword.php
@@ -4,30 +4,47 @@ declare(strict_types=1);
 
 namespace Slink\User\Domain\ValueObject\Auth;
 
-use SensitiveParameter;
-use const PASSWORD_BCRYPT;
-use function password_verify;
 use RuntimeException;
+use Slink\Shared\Domain\ValueObject\AbstractValueObject;
 
-final class HashedPassword implements \Stringable {
-  public const COST = 12;
-
-  private function __construct(private readonly string $hashedPassword) {
+final readonly class HashedPassword extends AbstractValueObject {
+  private const int COST = 12;
+  
+  /**
+   * @param string $hashedPassword
+   */
+  private function __construct(private string $hashedPassword) {
   }
-
-  public static function encode(#[SensitiveParameter] string $plainPassword): self {
+  
+  /**
+   * @param string $plainPassword
+   * @return self
+   */
+  public static function encode(#[\SensitiveParameter] string $plainPassword): self {
     return new self(self::hash($plainPassword));
   }
-
+  
+  /**
+   * @param string $hashedPassword
+   * @return self
+   */
   public static function fromHash(string $hashedPassword): self {
     return new self($hashedPassword);
   }
-
-  public function match(#[SensitiveParameter] string $plainPassword): bool {
-    return password_verify($plainPassword, $this->hashedPassword);
+  
+  /**
+   * @param string $plainPassword
+   * @return bool
+   */
+  public function match(#[\SensitiveParameter] string $plainPassword): bool {
+    return \password_verify($plainPassword, $this->hashedPassword);
   }
-
-  private static function hash(#[SensitiveParameter] string $plainPassword): string {
+  
+  /**
+   * @param string $plainPassword
+   * @return string
+   */
+  private static function hash(#[\SensitiveParameter] string $plainPassword): string {
     if (\mb_strlen($plainPassword) < 6) {
       throw new \InvalidArgumentException('Min 6 characters password');
     }
@@ -35,18 +52,18 @@ final class HashedPassword implements \Stringable {
     /** @var string|bool|null $hashedPassword */
     $hashedPassword = \password_hash($plainPassword, PASSWORD_BCRYPT, ['cost' => self::COST]);
 
-    if (\is_bool($hashedPassword)) {
+    if (!$hashedPassword) {
       throw new RuntimeException('Server error hashing password');
     }
 
     return (string) $hashedPassword;
   }
-
+  
+  /**
+   * @return string
+   */
+  #[\Override]
   public function toString(): string {
-    return $this->hashedPassword;
-  }
-
-  public function __toString(): string {
     return $this->hashedPassword;
   }
 }
