b8c1b3d66aa63174dfb8ea945770c99b70557b17
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-15 21:58:59 +0200

    Add type hints and docblocks to AuthProviderInterface

--- src/Slink/User/Infrastructure/Auth/AuthProviderInterface.php
+++ src/Slink/User/Infrastructure/Auth/AuthProviderInterface.php
@@ -9,7 +9,17 @@ use Slink\User\Domain\ValueObject\Email;
 use Ramsey\Uuid\UuidInterface;
 
 interface AuthProviderInterface {
+  /**
+   * @param UuidInterface $uuid
+   * @param Email $email
+   * @param HashedPassword $hashedPassword
+   * @return string
+   */
   public function generateAccessToken(UuidInterface $uuid, Email $email, HashedPassword $hashedPassword): string;
   
-  public function generateRefreshToken(?int $ttl): string;
+  /**
+   * @param int|null $ttl
+   * @return string
+   */
+  public function generateRefreshToken(?int $ttl = 2592000): string;
 }
