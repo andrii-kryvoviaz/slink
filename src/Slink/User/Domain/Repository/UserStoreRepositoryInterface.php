aa6d26d9bcf0a26f8495a9514461ab597a470af2
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-17 01:14:42 +0200

    Add getByUsername method to UserStore

--- src/Slink/User/Domain/Repository/UserStoreRepositoryInterface.php
+++ src/Slink/User/Domain/Repository/UserStoreRepositoryInterface.php
@@ -6,9 +6,12 @@ namespace Slink\User\Domain\Repository;
 
 use Slink\Shared\Domain\ValueObject\ID;
 use Slink\User\Domain\User;
+use Slink\User\Domain\ValueObject\Email;
 
 interface UserStoreRepositoryInterface {
   public function get(ID $id): User;
+  
+  public function getByUsername(Email $username): ?User;
 
   public function store(User $user): void;
 }
--- src/Slink/User/Infrastructure/Repository/UserStore.php
+++ src/Slink/User/Infrastructure/Repository/UserStore.php
@@ -6,14 +6,29 @@ namespace Slink\User\Infrastructure\Repository;
 
 use Slink\Shared\Domain\ValueObject\ID;
 use Slink\Shared\Infrastructure\Persistence\EventStore\AbstractStoreRepository;
+use Slink\User\Domain\Repository\CheckUserByEmailInterface;
 use Slink\User\Domain\Repository\UserStoreRepositoryInterface;
 use Slink\User\Domain\User;
+use Slink\User\Domain\ValueObject\Email;
+use Symfony\Contracts\Service\Attribute\Required;
 
 final class UserStore extends AbstractStoreRepository implements UserStoreRepositoryInterface {
+  private CheckUserByEmailInterface $checkUserByEmail;
+  
+  #[Required]
+  public function setCheckUserByEmail(CheckUserByEmailInterface $checkUserByEmail): void {
+    $this->checkUserByEmail = $checkUserByEmail;
+  }
+  
   static function getAggregateRootClass(): string {
     return User::class;
   }
 
+  /**
+   * @param ID $id
+   * @return User
+   */
+  #[\Override]
   public function get(ID $id): User {
     $user = $this->retrieve($id);
     if (!$user instanceof User) {
@@ -22,6 +37,26 @@ final class UserStore extends AbstractStoreRepository implements UserStoreReposi
     return $user;
   }
 
+  /**
+   * @param Email $username
+   * @return User|null
+   * @throws \RuntimeException
+   */
+  #[\Override]
+  public function getByUsername(Email $username): ?User {
+    $id = $this->checkUserByEmail->existsEmail($username);
+    if ($id === null) {
+      return null;
+    }
+    
+    return $this->get(ID::fromString($id->toString()));
+  }
+  
+  /**
+   * @param User $user
+   * @return void
+   */
+  #[\Override]
   public function store(User $user): void {
     $this->persist($user);
   }
