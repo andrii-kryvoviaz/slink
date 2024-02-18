a97c63318901666560eb7b845cb836594662888a
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-19 00:18:25 +0200

    Add Serializer Groups to UserView

--- src/Slink/User/Infrastructure/ReadModel/View/UserView.php
+++ src/Slink/User/Infrastructure/ReadModel/View/UserView.php
@@ -11,6 +11,7 @@ use Slink\Shared\Infrastructure\Persistence\ReadModel\AbstractView;
 use Slink\User\Domain\ValueObject\Auth\HashedPassword;
 use Slink\User\Domain\ValueObject\Email;
 use Slink\User\Infrastructure\ReadModel\Repository\UserRepository;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Table(name: '`user`')]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -18,18 +19,23 @@ final class UserView extends AbstractView {
   public function __construct(
     #[ORM\Id]
     #[ORM\Column(type: 'uuid')]
+    #[Groups(['public', 'internal'])]
     private readonly string $uuid,
 
     #[ORM\Column(type: 'email', unique: true)]
+    #[Groups(['public', 'internal'])]
     private Email $email,
 
     #[ORM\Column(type: 'hashed_password')]
+    #[Groups(['internal'])]
     private HashedPassword $password,
 
     #[ORM\Column(type: 'datetime_immutable')]
+    #[Groups(['internal'])]
     private readonly DateTime $createdAt,
 
     #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    #[Groups(['internal'])]
     private ?DateTime $updatedAt,
   ) {
   }
@@ -47,23 +53,23 @@ final class UserView extends AbstractView {
       isset($payload['updatedAt']) ? DateTime::fromString($payload['updatedAt']) : null,
     );
   }
-
+  
   public function getUuid(): string {
     return $this->uuid;
   }
-
-  public function getEmail(): Email {
-    return $this->email;
+  
+  public function getEmail(): string {
+    return $this->email->toString();
   }
-
+  
   public function getPassword(): HashedPassword {
     return $this->password;
   }
-
+  
   public function getCreatedAt(): DateTime {
     return $this->createdAt;
   }
-
+  
   public function getUpdatedAt(): ?DateTime {
     return $this->updatedAt;
   }
