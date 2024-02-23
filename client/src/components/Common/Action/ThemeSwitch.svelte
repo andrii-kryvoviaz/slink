4f305540292dff1c8ff1366f0213757ea03bde01
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-23 16:20:45 +0200

    Refactor Toggle component

--- client/src/components/Common/Action/ThemeSwitch.svelte
+++ client/src/components/Common/Action/ThemeSwitch.svelte
@@ -6,13 +6,15 @@
   import { Toggle } from '@slink/components/Form';
 
   export let disabled = false;
-
-  const toggleTheme = (e: Event) => {
-    setTheme((e.target as HTMLInputElement).checked ? Theme.DARK : Theme.LIGHT);
-  };
 </script>
 
-<Toggle on:change={toggleTheme} checked={$isDarkTheme} {disabled}>
+<Toggle
+  on:change={({ detail }) => {
+    setTheme(detail ? Theme.DARK : Theme.LIGHT);
+  }}
+  checked={$isDarkTheme}
+  {disabled}
+>
   <Icon slot="pre-icon" icon="ph:sun-thin" width="20" height="20" />
   <Icon slot="post-icon" icon="ph:moon-thin" width="20" height="20" />
 </Toggle>
--- client/src/components/Form/Toggle.svelte
+++ client/src/components/Form/Toggle.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  import type { HTMLInputAttributes } from 'svelte/elements';
+
+  interface $$Props extends HTMLInputAttributes {}
+
   const inputClasses = `toggle theme-controller text-toggle-default checked:text-toggle-checked [--tglbg:rgb(var(--bg-toggle))] checked:[--tglbg:rgb(var(--bg-toggle-checked))] ${$$props.class}`;
+
+  const dispatch = createEventDispatcher<{ change: boolean }>();
+  const handleChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+
+    dispatch('change', target.checked);
+  };
 </script>
 
 <label class="flex cursor-pointer items-center gap-2">
   <slot name="pre-icon" />
-  <input {...$$props} type="checkbox" class={inputClasses} on:change />
+  <input
+    {...$$props}
+    type="checkbox"
+    class={inputClasses}
+    on:change={handleChange}
+  />
   <slot name="post-icon" />
 </label>
