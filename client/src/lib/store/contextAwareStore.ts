1915d4adc298f3f7c8042717413861638fda7f3d
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-24 14:21:31 +0200

    Add context-aware store to solve shared state issue on the server

---
+++ client/src/lib/store/contextAwareStore.ts
@@ -0,0 +1,22 @@
+import { getContext, hasContext, setContext } from 'svelte';
+
+import { type Readable, type Writable, readable, writable } from 'svelte/store';
+
+export const useSharedStore = <T, A>(
+  name: string,
+  fn: (value?: A) => T,
+  defaultValue?: A
+): T => {
+  if (hasContext(name)) {
+    return getContext<T>(name);
+  }
+  const _value = fn(defaultValue);
+  setContext(name, _value);
+  return _value;
+};
+
+export const useWritable = <T>(name: string, value: T): Writable<T> =>
+  useSharedStore<Writable<T>, T>(name, writable, value);
+
+export const useReadable = <T>(name: string, value: T): Readable<T> =>
+  useSharedStore<Readable<T>, T>(name, readable, value);
