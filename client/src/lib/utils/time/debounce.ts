05a54da7e2bb72ea7dc2fdcd65769d0e9d2d9928
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-25 18:27:25 +0200

    Refactor debounce function to be asynchronous

--- client/src/lib/utils/time/debounce.ts
+++ client/src/lib/utils/time/debounce.ts
@@ -1,6 +1,6 @@
 export function debounce(fn: Function, ms = 0) {
   let timeoutId: number;
-  return function (...args: any[]) {
+  return async function (...args: any[]) {
     clearTimeout(timeoutId);
     // @ts-ignore
     timeoutId = window.setTimeout(() => fn.apply(this, args), ms);
