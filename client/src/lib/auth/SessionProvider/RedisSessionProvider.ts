ca78ec8ac393d0045f7f1c4a393d8e9611451330
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-02-28 00:13:16 +0200

    Refactor RedisSessionProvider class to handle connection status

--- client/src/lib/auth/SessionProvider/RedisSessionProvider.ts
+++ client/src/lib/auth/SessionProvider/RedisSessionProvider.ts
@@ -3,14 +3,15 @@ import { type RedisClientType, createClient } from 'redis';
 
 export class RedisSessionProvider implements SessionProviderInterface {
   private static _instance: RedisSessionProvider;
+
+  private connected: boolean = false;
   private defaultTtl: number = 60 * 60 * 24 * 7;
   private prefix: string = 'session:';
+
   private constructor(private _redis: RedisClientType) {}
 
   public static async createClient() {
-    const redis = await createClient()
-      .on('error', (err) => console.error('Redis Client Error', err))
-      .connect();
+    const redis = createClient();
 
     return (
       this._instance ??
@@ -18,7 +19,23 @@ export class RedisSessionProvider implements SessionProviderInterface {
     );
   }
 
+  private async _connectIfNotConnected() {
+    if (!this.connected) {
+      this._redis
+        .on('connect', () => {
+          this.connected = true;
+        })
+        .on('error', () => {
+          this.connected = false;
+        });
+
+      await this._redis.connect();
+    }
+  }
+
   async create(sessionId: string, ttl: number | null = null): Promise<void> {
+    await this._connectIfNotConnected();
+
     await this._redis.set(
       this.prefix + sessionId,
       JSON.stringify({ data: { user: null } })
@@ -27,6 +44,8 @@ export class RedisSessionProvider implements SessionProviderInterface {
   }
 
   async get<T>(sessionId: string): Promise<T | undefined> {
+    await this._connectIfNotConnected();
+
     const session = await this._redis.get(this.prefix + sessionId);
 
     if (!session) return;
@@ -39,11 +58,15 @@ export class RedisSessionProvider implements SessionProviderInterface {
     data: T,
     ttl: number | null = null
   ): Promise<void> {
+    await this._connectIfNotConnected();
+
     await this._redis.set(this.prefix + sessionId, JSON.stringify(data));
     await this._redis.expire(this.prefix + sessionId, ttl ?? this.defaultTtl);
   }
 
   async destroy(sessionId: string): Promise<void> {
+    await this._connectIfNotConnected();
+
     await this._redis.del(this.prefix + sessionId);
   }
 }
