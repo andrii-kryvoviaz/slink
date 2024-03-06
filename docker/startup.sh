9a0883ba14826b6a43d7bb342d13add501ed7ac0
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-03-07 01:49:20 +0200

    Add JWT keys generation to the startup script

--- docker/startup.sh
+++ docker/startup.sh
@@ -9,3 +9,6 @@
 # Apply migrations
 /app/bin/console doctrine:migrations:migrate --no-interaction --configuration=/app/config/migrations/event_store.yaml --em=event_store
 /app/bin/console doctrine:migrations:migrate --no-interaction --em=read_model
+
+# Generate JWT keys
+/app/bin/console lexik:jwt:generate-keypair --skip-if-exists
