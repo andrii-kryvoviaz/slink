ffa2ff578a6aee344c4faa82d1c06041dba6f6a6
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-03-29 19:47:15 +0200

    Refactor iconify extranxtion plugin regexp

--- client/plugins/vite-iconify-export/helpers.ts
+++ client/plugins/vite-iconify-export/helpers.ts
@@ -1,23 +1,35 @@
 const RegexPatterns = {
   VARIABLE_DECLARATION: /.*(?=\s=\s\[)/,
   ICON_LINE: /'([^']+)'/g,
-  ICON_TAG: /(Icon[.\n\s]*)?icon[=:\s]+(["'])([^"']+:[^"']+)\2/g
+  ICON_TAG:
+    /(?:Icon[.\n\s]*)?(?:icon|\s:)[=:\s]+.*?["'](?<icon>[a-z0-9\-]+:[a-z0-9\-]+)["']/g,
 };
 
 type RegexPattern = keyof typeof RegexPatterns;
 
 type RegexMatchOptions = {
-  group?: number,
-  defaultValue?: any
-}
+  group?: number;
+  defaultValue?: any;
+};
 
 export class RegexHelper {
-  static extractRegexGroupsToArray(regexPattern: RegexPattern, content: string, { group = 1, defaultValue = [] }: RegexMatchOptions = {}): string[] {
+  static extractRegexGroupsToArray(
+    regexPattern: RegexPattern,
+    content: string,
+    { group = 1, defaultValue = [] }: RegexMatchOptions = {}
+  ): string[] {
     const regex = this.getRegexPattern(regexPattern);
-    return content.match(regex)?.map((match) => match.replace(regex, `$${group}`)) || defaultValue;
+    return (
+      content.match(regex)?.map((match) => match.replace(regex, `$${group}`)) ||
+      defaultValue
+    );
   }
 
-  static matchString(regexPattern: RegexPattern, content: string, { group = 1, defaultValue = [] }: RegexMatchOptions = {}): string {
+  static matchString(
+    regexPattern: RegexPattern,
+    content: string,
+    { group = 1, defaultValue = [] }: RegexMatchOptions = {}
+  ): string {
     const regex = this.getRegexPattern(regexPattern);
     return content.match(regex)?.[group] || defaultValue;
   }
--- client/plugins/vite-iconify-export/lib/IconFile.ts
+++ client/plugins/vite-iconify-export/lib/IconFile.ts
@@ -1,4 +1,5 @@
 import fs from 'fs';
+
 import { RegexHelper } from '../helpers';
 import { IconRegistry } from './IconRegistry';
 
@@ -8,14 +9,20 @@ export class IconFile {
 
   private constructor(private readonly filePath: string) {
     const content = fs.readFileSync(this.filePath, 'utf-8');
-    this.iconRegistry = IconRegistry.create(RegexHelper.extractRegexGroupsToArray('ICON_LINE', content));
-    this.variableDeclaration = RegexHelper.matchString('VARIABLE_DECLARATION', content, {
-      defaultValue: 'export const icons',
-      group: 0
-    });
+    this.iconRegistry = IconRegistry.create(
+      RegexHelper.extractRegexGroupsToArray('ICON_LINE', content)
+    );
+    this.variableDeclaration = RegexHelper.matchString(
+      'VARIABLE_DECLARATION',
+      content,
+      {
+        defaultValue: 'export const icons',
+        group: 0,
+      }
+    );
   }
 
-  public static create(filePath: string): IconFile{
+  public static create(filePath: string): IconFile {
     return new IconFile(filePath);
   }
 
@@ -25,16 +32,22 @@ export class IconFile {
     }
 
     const content = fs.readFileSync(filePath, 'utf-8');
-    const parsedIcons = RegexHelper.extractRegexGroupsToArray('ICON_TAG', content, {
-      group: 3
-    });
+    const parsedIcons = RegexHelper.extractRegexGroupsToArray(
+      'ICON_TAG',
+      content,
+      {
+        group: 1,
+      }
+    );
+
+    console.log(parsedIcons);
 
     if (!parsedIcons.length) {
       return;
     }
 
     parsedIcons.forEach((icon) => {
-      if(!this.iconRegistry.hasIcon(icon)) {
+      if (!this.iconRegistry.hasIcon(icon)) {
         this.iconRegistry.trackChanges();
       }
       this.iconRegistry.addLink(icon, filePath);
@@ -45,7 +58,9 @@ export class IconFile {
 
   persist() {
     const icons = this.iconRegistry.getPersistentIcons();
-    const content = `${this.variableDeclaration} = [\n  '${icons.join("',\n  '")}',\n];`;
+    const content = `${this.variableDeclaration} = [\n  '${icons.join(
+      "',\n  '"
+    )}',\n];\n`;
     if (this.iconRegistry.hasChanged()) {
       fs.writeFileSync(this.filePath, content);
     }
