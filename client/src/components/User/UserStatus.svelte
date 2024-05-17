eb4c306cb0ea11b86da407ef8f5e9d0a801edc2b
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-05-18 00:25:46 +0300

    Add UserStatus component

---
+++ client/src/components/User/UserStatus.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { UserStatus } from '@slink/lib/auth/Type/User';
+
+  import { Badge, type BadgeProps } from '@slink/components/Content';
+
+  interface $$Props extends BadgeProps {
+    status: UserStatus;
+  }
+
+  export let status: UserStatus = UserStatus.Active;
+
+  $: {
+    if (status === UserStatus.Active) {
+      $$props.variant = 'accent';
+    } else if (status === UserStatus.Inactive) {
+      $$props.variant = 'neutral';
+    } else if (status === UserStatus.Suspended) {
+      $$props.variant = 'secondary';
+    } else if (status === UserStatus.Banned) {
+      $$props.variant = 'danger';
+    } else {
+      $$props.variant = 'default';
+    }
+  }
+</script>
+
+<Badge size="sm" outline="true" {...$$props}>
+  <span class="capitalize">{status}</span>
+</Badge>
