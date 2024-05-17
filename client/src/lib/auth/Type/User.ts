8f6c6cc4898457c80cee21da4e0a51847b983965
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-05-18 00:25:46 +0300

    Add UserStatus component

--- client/src/lib/auth/Type/User.ts
+++ client/src/lib/auth/Type/User.ts
@@ -1,6 +1,16 @@
+// export type UserStatus = 'active' | 'inactive' | 'suspended' | 'banned';
+
+export enum UserStatus {
+  Active = 'active',
+  Inactive = 'inactive',
+  Suspended = 'suspended',
+  Banned = 'banned',
+}
+
 export type User = {
   id: string;
   email: string;
   displayName: string;
   roles: string[];
+  status?: UserStatus;
 };
