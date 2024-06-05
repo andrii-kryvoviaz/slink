c329f795553d9b6f3fd29573eae07bc202b6ed37
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-06-05 23:11:07 +0300

    refactor: Update DateInterval enum values to use snake_case

--- src/Slink/Shared/Domain/Enum/Date/DateInterval.php
+++ src/Slink/Shared/Domain/Enum/Date/DateInterval.php
@@ -5,12 +5,25 @@ declare(strict_types=1);
 namespace Slink\Shared\Domain\Enum\Date;
 
 enum DateInterval: string{
-  case CURRENT_WEEK = 'Current Week';
-  case LAST_WEEK = 'Last Week';
-  case LAST_7_DAYS = 'Last 7 Days';
-  case CURRENT_MONTH = 'Current Month';
-  case LAST_MONTH = 'Last Month';
-  case LAST_30_DAYS = 'Last 30 Days';
-  case CURRENT_YEAR = 'Current Year';
-  case ALL_TIME = 'All Time';
+  case CURRENT_WEEK = 'current_week';
+  case LAST_WEEK = 'last_week';
+  case LAST_7_DAYS = '7_days';
+  case CURRENT_MONTH = 'current_month';
+  case LAST_MONTH = 'last_month';
+  case LAST_30_DAYS = '30_days';
+  case CURRENT_YEAR = 'current_year';
+  case ALL_TIME = 'all_time';
+  
+  public function toString(): string {
+    return match ($this) {
+      self::CURRENT_WEEK => 'Current Week',
+      self::LAST_WEEK => 'Last Week',
+      self::LAST_7_DAYS => 'Last 7 Days',
+      self::CURRENT_MONTH => 'Current Month',
+      self::LAST_MONTH => 'Last Month',
+      self::LAST_30_DAYS => 'Last 30 Days',
+      self::CURRENT_YEAR => 'Current Year',
+      self::ALL_TIME => 'All Time',
+    };
+  }
 }
