0a5de75d61488ae96c65b8a415fb288b510fbf40
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-06-05 22:18:32 +0300

    Add user analytics controller

---
+++ src/Slink/User/Application/Query/User/GetUserAnalytics/GetUserAnalyticsQuery.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Slink\User\Application\Query\User\GetUserAnalytics;
+
+use Slink\Shared\Application\Query\QueryInterface;
+
+final readonly class GetUserAnalyticsQuery implements QueryInterface {
+  public function __construct(
+    private string $key
+  ) {
+  }
+  
+  public function key(): string {
+    return $this->key;
+  }
+}
