14a8c516a894f695eb507309e8c71d953298c0a7
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2024-12-28 03:06:16 +0200

    fix: Update Dropdown component styles

--- client/src/components/UI/Form/Dropdown/Dropdown.svelte
+++ client/src/components/UI/Form/Dropdown/Dropdown.svelte
@@ -97,7 +97,7 @@
   const mobileContentClasses = 'fixed inset-0 m-auto';
 
   const baseContentClasses =
-    'md:absolute md:inset-auto md:m-1 z-50 mx-auto w-fit min-w-56 origin-top-left max-h-fit divide-y divide-gray-100 rounded-md bg-white font-light shadow-lg ring-1 ring-black ring-opacity-5 dark:divide-zinc-700 dark:bg-zinc-900';
+    'md:absolute md:inset-auto md:m-1 z-50 mx-auto w-fit min-w-56 origin-top-left max-h-fit divide-y divide-gray-100 rounded-md bg-white font-light shadow-lg ring-1 ring-black ring-opacity-5 dark:divide-zinc-700 dark:divide-neutral-700/30 dark:bg-neutral-950 border-neutral-100/10 border';
 
   const classMap = {
     'bottom-right': 'md:top-full md:right-0',
