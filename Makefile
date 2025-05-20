0c3129eb089c5fdb9b46d6a2daf4468dff5371cf
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2025-05-20 08:13:20 +0300

    chore: Refactor project structure

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 run:
-	docker compose -f docker-compose.yaml -f docker-compose.prod.yaml up -d --build
+	docker compose -p slink -f docker/docker-compose.yaml -f docker/docker-compose.prod.yaml up -d --build
 
 run-dev:
-	docker compose -f docker-compose.yaml -f docker-compose.dev.yaml up -d --build;
+	docker compose -p slink -f docker/docker-compose.yaml -f docker/docker-compose.dev.yaml up -d --build;
 	docker attach slink
 
 purge:
-	docker compose -f docker-compose.yaml down --rmi all --volumes --remove-orphans
+	docker compose -f docker/docker-compose.yaml down --rmi all --volumes --remove-orphans
 
 release:
 	@if [ -z $(version) ]; then echo "Version is required. Usage: make release version=x.x.x"; exit 1; fi
--- docker/scripts/startup.sh
+++ docker/scripts/startup.sh
@@ -11,19 +11,19 @@ if [[ "$DATABASE_URL" == sqlite:* ]]; then
   echo "[Startup] Skipping creation of Projection Database as DATABASE_URL uses SQLite."
 else
   # Create Projection Database
-  /app/bin/console doctrine:database:create --if-not-exists --no-interaction --env=prod --connection=read_model
+  slink doctrine:database:create --if-not-exists --no-interaction --env=prod --connection=read_model
 fi
 
 if [[ "$ES_DATABASE_URL" == sqlite:* ]]; then
   echo "[Startup] Skipping creation of Event Store Database as ES_DATABASE_URL uses SQLite."
 else
   # Create Event Store Database
-  /app/bin/console doctrine:database:create --if-not-exists --no-interaction --env=prod --connection=event_store
+  slink doctrine:database:create --if-not-exists --no-interaction --env=prod --connection=event_store
 fi
 
 # Apply migrations
-/app/bin/console doctrine:migrations:migrate --no-interaction --configuration=/app/config/migrations/event_store.yaml --em=event_store
-/app/bin/console doctrine:migrations:migrate --no-interaction --em=read_model
+slink doctrine:migrations:migrate --no-interaction --configuration=/apps/api/config/migrations/event_store.yaml --em=event_store
+slink doctrine:migrations:migrate --no-interaction --em=read_model
 
 # Generate JWT keys
-/app/bin/console lexik:jwt:generate-keypair --skip-if-exists
+slink lexik:jwt:generate-keypair --skip-if-exists
