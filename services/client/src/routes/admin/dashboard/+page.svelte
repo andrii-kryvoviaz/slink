048b56273f45c7294210b59f23a449beddc1f784
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2025-06-24 23:28:04 +0300

    refactor: Adjust grid layout in dashboard for improved responsiveness

--- services/client/src/routes/admin/dashboard/+page.svelte
+++ services/client/src/routes/admin/dashboard/+page.svelte
@@ -13,15 +13,15 @@
 </svelte:head>
 
 <section in:fade={{ duration: 300 }} class="relative h-full grow py-4">
-  <div class="grid h-full grid-cols-2 content-start gap-6 px-6">
-    <div class="col-span-2 h-96 md:col-span-1 xl:h-[40rem]">
+  <div class="grid h-full grid-cols-1 md:grid-cols-5 content-start gap-6 px-6">
+    <div class="col-span-1 md:col-span-3 h-96 xl:h-[40rem]">
       <ImageAnalytics />
     </div>
-    <div class="col-span-2 h-96 md:col-span-1 xl:h-[40rem]">
+    <div class="col-span-1 md:col-span-2 h-96 xl:h-[40rem]">
       <UserAnalytics />
     </div>
 
-    <div class="col-span-2">
+    <div class="col-span-1 md:col-span-5">
       <LatestUsers />
     </div>
   </div>
