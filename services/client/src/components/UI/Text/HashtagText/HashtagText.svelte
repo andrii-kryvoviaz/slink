523dc9dde6c932e98c927de10b924fe8670530ea
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2025-06-25 22:16:24 +0300

    refactor: Remove HashtagHandler component and integrate functionality into HashtagText component

--- services/client/src/components/UI/Text/HashtagText/HashtagText.svelte
+++ services/client/src/components/UI/Text/HashtagText/HashtagText.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-  import { splitTextIntoSegments } from '@slink/lib/utils/text/hashtag';
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
+
+  import { usePublicImagesFeed } from '@slink/lib/state/PublicImagesFeed.svelte';
+  import {
+    createHashtagSearchQuery,
+    splitTextIntoSegments,
+  } from '@slink/lib/utils/text/hashtag';
   import { className } from '@slink/lib/utils/ui/className';
 
-  import HashtagHandler from '../HashtagHandler/HashtagHandler.svelte';
   import { type HashtagVariant, hashtagVariants } from './HashtagText.theme';
 
   interface Props extends HashtagVariant {
@@ -21,27 +27,45 @@
 
   const segments = $derived(splitTextIntoSegments(text));
   const hashtagClasses = $derived(hashtagVariants({ variant, size, rounded }));
+  const publicFeedState = usePublicImagesFeed();
+
+  const handleHashtagClick = (hashtag: string): void => {
+    const searchQuery = createHashtagSearchQuery(hashtag);
+
+    if (page.route.id === '/explore') {
+      publicFeedState.search(searchQuery, 'hashtag');
+    } else {
+      goto(
+        `/explore?search=${encodeURIComponent(searchQuery)}&searchBy=hashtag`,
+      );
+    }
+  };
+
+  const handleKeyDown = (event: KeyboardEvent, hashtag: string): void => {
+    if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      handleHashtagClick(hashtag);
+    }
+  };
 </script>
 
-<HashtagHandler>
-  {#snippet children()}
-    <span class={classNameProp}>
-      {#each segments as segment}
-        {#if segment.isHashtag && segment.hashtag}
-          <span
-            class={className(hashtagClasses)}
-            data-hashtag={segment.hashtag}
-            title="Click to search for {segment.text}"
-            role="button"
-            tabindex="0"
-            {...props}
-          >
-            {segment.text}
-          </span>
-        {:else}
-          {segment.text}
-        {/if}
-      {/each}
-    </span>
-  {/snippet}
-</HashtagHandler>
+<span class={classNameProp}>
+  {#each segments as segment}
+    {#if segment.isHashtag && segment.hashtag}
+      <span
+        class={className(hashtagClasses)}
+        data-hashtag={segment.hashtag}
+        title="Click to search for {segment.text}"
+        role="button"
+        tabindex="0"
+        onclick={() => handleHashtagClick(segment.hashtag!)}
+        onkeydown={(event) => handleKeyDown(event, segment.hashtag!)}
+        {...props}
+      >
+        {segment.text}
+      </span>
+    {:else}
+      {segment.text}
+    {/if}
+  {/each}
+</span>
