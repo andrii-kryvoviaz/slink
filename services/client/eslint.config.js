bcba5f3f7496f4c44704dfc871514f30d627b3ff
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2025-09-13 12:32:43 +0300

    chore: Add husky and integrate it with linting

---
+++ services/client/eslint.config.js
@@ -0,0 +1,87 @@
+import js from '@eslint/js';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import svelteParser from 'svelte-eslint-parser';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    ignores: ['**/*.svelte.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+  {
+    files: ['**/*.{svelte,svelte.ts}'],
+    languageOptions: {
+      parser: svelteParser,
+      parserOptions: {
+        parser: tsParser,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      svelte,
+      '@typescript-eslint': ts,
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'svelte/no-unused-svelte-ignore': 'error',
+      'svelte/no-at-html-tags': 'off',
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+  {
+    ignores: [
+      'build/',
+      '.svelte-kit/',
+      'dist/',
+      'node_modules/',
+      '*.config.js',
+      '*.config.ts',
+    ],
+  },
+  prettier,
+];
