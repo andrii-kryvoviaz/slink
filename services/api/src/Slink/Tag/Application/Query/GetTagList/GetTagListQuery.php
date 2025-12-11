13ec5c686408657f677b9604866dc0d7724784fe
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2025-12-11 14:23:27 +0200

    fix: update Assert\Choice annotations usage

--- services/api/src/Slink/Tag/Application/Query/GetTagList/GetTagListQuery.php
+++ services/api/src/Slink/Tag/Application/Query/GetTagList/GetTagListQuery.php
@@ -18,10 +18,10 @@ final readonly class GetTagListQuery implements QueryInterface {
     #[Assert\Range(min: 1, max: 100)]
     private ?int    $limit = 50,
 
-    #[Assert\Choice(['name', 'path', 'createdAt', 'updatedAt'])]
+    #[Assert\Choice(choices: ['name', 'path', 'createdAt', 'updatedAt'])]
     private ?string $orderBy = 'name',
 
-    #[Assert\Choice(['asc', 'desc'])]
+    #[Assert\Choice(choices: ['asc', 'desc'])]
     private ?string $order = 'asc',
 
     #[Assert\Range(min: 1)]
