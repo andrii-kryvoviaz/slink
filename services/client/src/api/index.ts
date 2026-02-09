419c5e62541f64cb218ae5504349228c8c340495
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2026-02-09 14:03:33 +0200

    fix: add missing index.ts

---
+++ services/client/src/api/index.ts
@@ -0,0 +1,5 @@
+export { ApiClient } from './ApiClient';
+export { ApiRequestBuilder } from './ApiRequestBuilder';
+export { ApiError, TokenRefreshError, handleApiError } from './ApiError';
+export { API_CLIENT_BRAND, createApiClient } from './Client';
+export type { ApiClientType } from './Client';
