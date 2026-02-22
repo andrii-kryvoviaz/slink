52300b4edd983817c3343604d3f7c69e84878b70
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2026-02-22 22:12:22 +0200

    refactor: rename Admin image controllers to remove redundant prefix

--- services/api/src/UI/Http/Rest/Controller/Admin/Image/DeleteImageController.php
+++ services/api/src/UI/Http/Rest/Controller/Admin/Image/DeleteImageController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace UI\Http\Rest\Controller\Admin\Image;
 
-use Slink\Image\Application\Command\UpdateImage\AdminUpdateImageCommand;
+use Slink\Image\Application\Command\DeleteImage\AdminDeleteImageCommand;
 use Slink\Shared\Application\Command\CommandTrait;
 use Slink\User\Domain\Enum\UserRole;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -14,19 +14,19 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 use UI\Http\Rest\Response\ApiResponse;
 
 #[AsController]
-#[Route(path: '/admin/image/{id}', name: 'admin_update_image', methods: ['PATCH'])]
+#[Route(path: '/admin/image/{id}', name: 'admin_delete_image', methods: ['DELETE'])]
 #[IsGranted(UserRole::Admin->value)]
-final readonly class AdminUpdateImageController {
+final class DeleteImageController {
   use CommandTrait;
-  
+
   public function __invoke(
-    #[MapRequestPayload] AdminUpdateImageCommand $command,
-    string $id
+    #[MapRequestPayload] AdminDeleteImageCommand $command,
+    string $id,
   ): ApiResponse {
     $this->handle($command->withContext([
       'id' => $id,
     ]));
-    
+
     return ApiResponse::empty();
   }
 }
--- services/api/src/UI/Http/Rest/Controller/Admin/Image/UpdateImageController.php
+++ services/api/src/UI/Http/Rest/Controller/Admin/Image/UpdateImageController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace UI\Http\Rest\Controller\Admin\Image;
 
-use Slink\Image\Application\Command\DeleteImage\AdminDeleteImageCommand;
+use Slink\Image\Application\Command\UpdateImage\AdminUpdateImageCommand;
 use Slink\Shared\Application\Command\CommandTrait;
 use Slink\User\Domain\Enum\UserRole;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -14,19 +14,19 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 use UI\Http\Rest\Response\ApiResponse;
 
 #[AsController]
-#[Route(path: '/admin/image/{id}', name: 'admin_delete_image', methods: ['DELETE'])]
+#[Route(path: '/admin/image/{id}', name: 'admin_update_image', methods: ['PATCH'])]
 #[IsGranted(UserRole::Admin->value)]
-final class AdminDeleteImageController {
+final readonly class UpdateImageController {
   use CommandTrait;
-  
+
   public function __invoke(
-    #[MapRequestPayload] AdminDeleteImageCommand $command,
-    string $id,
+    #[MapRequestPayload] AdminUpdateImageCommand $command,
+    string $id
   ): ApiResponse {
     $this->handle($command->withContext([
       'id' => $id,
     ]));
-    
+
     return ApiResponse::empty();
   }
 }
