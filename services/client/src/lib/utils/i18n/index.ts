805a851ef342461ff6e14f71167a85108af54933
Andrii Kryvoviaz <andy.krivovjas@gmail.com>
2026-09-09 20:45:23 +0200

    fix: use wuchale generated plural rules

--- services/client/src/lib/utils/i18n/index.ts
+++ services/client/src/lib/utils/i18n/index.ts
@@ -1,4 +1,4 @@
-export { plural } from './plural';
+export { default as plural } from '@slink/locales/plural.js';
 export { localize } from './localize';
 export { runtimeTranslator, t } from './RuntimeTranslator.svelte';
 export { applyLocale } from './applyLocale';
